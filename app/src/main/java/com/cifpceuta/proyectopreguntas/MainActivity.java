2f735cd44a386acda0aa978b317bb9da47c28657
9150FJ <9150@cifpceuta.es>
2023-11-30 06:25:55 +0100

    Actualizado

--- app/src/main/java/com/cifpceuta/proyectopreguntas/MainActivity.java
+++ app/src/main/java/com/cifpceuta/proyectopreguntas/MainActivity.java
@@ -2,13 +2,67 @@ package com.cifpceuta.proyectopreguntas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView contadorPregunta;
+    private TextView pregunta;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton1, radioButton2, radioButton3;
+    private Button botonSiguiente;
+    private PreguntaRespuesta[] preguntas = {
+            new PreguntaRespuesta("¿Cuál es la capital de Francia?", "París", "Madrid", "Londres", 1),
+            new PreguntaRespuesta("¿Cuántos planetas hay en el sistema solar?", "7", "8", "9", 2),
+
+    };
+    private int contadorGeneral = 0;
+    private int aciertos = 0;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contadorPregunta = findViewById(R.id.contadorPregunta);
+        pregunta = findViewById(R.id.pregunta);
+        radioGroup = findViewById(R.id.grupoBoton);
+        radioButton1 = findViewById(R.id.radioButton1);
+        radioButton2 = findViewById(R.id.radioButton2);
+        radioButton3 = findViewById(R.id.radioButton3);
+        botonSiguiente = findViewById(R.id.botonSiguiente);
+        mostrarPregunta();
+
+        botonSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //System.out.println(radioGroup.getCheckedRadioButtonId());
+                Log.d("Hola",radioGroup.getCheckedRadioButtonId()+"");
+
+
+            }
+        });
+
     }
+
+
+
+    private void mostrarPregunta() {
+        contadorPregunta.setText("Pregunta " + (contadorGeneral + 1) + "/" + preguntas.length);
+        pregunta.setText(preguntas[contadorGeneral].getPregunta());
+        radioButton1.setText(preguntas[contadorGeneral].getRespuesta1());
+        radioButton2.setText(preguntas[contadorGeneral].getRespuesta2());
+        radioButton3.setText(preguntas[contadorGeneral].getRespuesta3());
+
+    }
+
 }
--- app/src/main/java/com/cifpceuta/proyectopreguntas/PreguntaRespuesta.java
+++ app/src/main/java/com/cifpceuta/proyectopreguntas/PreguntaRespuesta.java
@@ -22,6 +22,40 @@ public class PreguntaRespuesta {
         return preguntaCorrecta;
     }
 
+    public String getPregunta() {
+        return pregunta;
+    }
 
+    public void setPregunta(String pregunta) {
+        this.pregunta = pregunta;
+    }
+
+    public String getRespuesta1() {
+        return respuesta1;
+    }
+
+    public void setRespuesta1(String respuesta1) {
+        this.respuesta1 = respuesta1;
+    }
+
+    public String getRespuesta2() {
+        return respuesta2;
+    }
+
+    public void setRespuesta2(String respuesta2) {
+        this.respuesta2 = respuesta2;
+    }
+
+    public String getRespuesta3() {
+        return respuesta3;
+    }
+
+    public void setRespuesta3(String respuesta3) {
+        this.respuesta3 = respuesta3;
+    }
+
+    public void setPreguntaCorrecta(int preguntaCorrecta) {
+        this.preguntaCorrecta = preguntaCorrecta;
+    }
 }
 
